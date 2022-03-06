8df0d7a7773d3498e2cc04b39cb46a597b4f224a
suriya7913 <suriya962927@gmail.com>
2022-03-06 12:31:23 +0530

    project done

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
+import imp
 from django.contrib import admin
-
+from .models import Room ,Message
 # Register your models here.
+admin.site.register(Room)
+admin.site.register(Message)
+#admin pass=12345
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
+from datetime import date
+from logging.config import valid_ident
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=1000)
+class Message(models.Model):
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.CharField(max_length=1000000)
+    room = models.CharField(max_length=1000000)
